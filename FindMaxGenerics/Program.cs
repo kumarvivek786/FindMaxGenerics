ddae8d4b8dc342d6b7e190addee7b2e5966b73f1
vivek <ikumarvivek786@gmail.com>
2022-10-08 22:47:26 +0530

    Uc-3.1Refractor

--- FindMaxGenerics/Program.cs
+++ FindMaxGenerics/Program.cs
@@ -10,14 +10,20 @@ namespace FindMaxGenerics
     {
         static void Main(string[] args)
         {
-            MaxNum maxnum = new MaxNum();
-            Console.WriteLine("test case 1");
-            Console.WriteLine("Largest String is " + MaxNum.MaximumStringNumber("Shreyas", "Abhi", "Megha"));
-            Console.WriteLine("test case 2");
-            Console.WriteLine("Largest String is " + MaxNum.MaximumStringNumber("Megha", "Sagar", "Aish"));
-            Console.WriteLine("test case 3");
-            Console.WriteLine("Largest String is " + MaxNum.MaximumStringNumber("Bharath", "Gowri", "Virat")); Console.ReadLine();
+            
+            {
+                MaxGeneric<int> maxGenericInt = new MaxGeneric<int>();
+                maxGenericInt.findMax(20, 60, 40);
 
+                MaxGeneric<float> maxGenericFloat = new MaxGeneric<float>();
+                maxGenericFloat.findMax(10.10f, 11.10f, 12.12f);
+
+                MaxGeneric<string> maxGenericStr = new MaxGeneric<string>();
+                maxGenericStr.findMax("Apple","Pineapple","Orange");
+                Console.ReadLine();
+
+
+            }
 
         }
     }
